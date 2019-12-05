5f7c253afd61304778b0e1a89f2e5044fbcdfadd
Nevette Bailey <nevettebailey@gmail.com>
2019-12-04 21:10:18 -0500

    algo: long pressed name optimized

--- 13_long-pressed-name.js
+++ 13_long-pressed-name.js
@@ -24,33 +24,67 @@ Example 4:
   Output: true
   Explanation: It's not necessary to long press any character.
  */
-/*eslint complexity: ["error", 20]*/
+
+/* optimized approach */
+/* count each letter, make a hash table, compare the values --> typed >= name */
 
 function isLongPressedName(name, typed) {
-  let namePointerIdx = 0;
-  let typedPointerIdx = 0;
-  let boolCheck = false;
-  while (typedPointerIdx < typed.length && namePointerIdx < name.length) {
-    if (
-      name[namePointerIdx] === typed[typedPointerIdx] &&
-      namePointerIdx === name.length - 1
-    ) {
-      return true;
-    } else if (name[namePointerIdx] === typed[typedPointerIdx]) {
-      typedPointerIdx++;
-      boolCheck = true;
-      if (typed[typedPointerIdx] === name[namePointerIdx + 1]) {
-        namePointerIdx++;
-      }
-    } else if (name[namePointerIdx] !== typed[typedPointerIdx] && boolCheck) {
-      namePointerIdx++;
-      boolCheck = false;
+  //initialize each letter in the name/typed with a key
+  //of the letter, and a value of one
+  let nameMap = new Map();
+  let typedMap = new Map();
+  for (let i = 0; i < name.length; i++) {
+    if (nameMap.has(name[i])) {
+      nameMap.set(name[i], nameMap.get(name[i]) + 1);
     } else {
-      return false;
+      nameMap.set(name[i], 1);
     }
   }
-  return false;
+  for (let j = 0; j < typed.length; j++) {
+    if (typedMap.has(typed[j])) {
+      typedMap.set(typed[j], typedMap.get(typed[j]) + 1);
+    } else {
+      typedMap.set(typed[j], 1);
+    }
+  }
+  if (nameMap.size !== typedMap.size) {
+    return false;
+  } else {
+    for (let [key, value] of typedMap.entries()) {
+      if (value < nameMap.get(key)) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
+
+/* brute force approach */
+// function isLongPressedName(name, typed) {
+//   let namePointerIdx = 0;
+//   let typedPointerIdx = 0;
+//   let boolCheck = false;
+//   while (typedPointerIdx < typed.length && namePointerIdx < name.length) {
+//     if (
+//       name[namePointerIdx] === typed[typedPointerIdx] &&
+//       namePointerIdx === name.length - 1
+//     ) {
+//       return true;
+//     } else if (name[namePointerIdx] === typed[typedPointerIdx]) {
+//       typedPointerIdx++;
+//       boolCheck = true;
+//       if (typed[typedPointerIdx] === name[namePointerIdx + 1]) {
+//         namePointerIdx++;
+//       }
+//     } else if (name[namePointerIdx] !== typed[typedPointerIdx] && boolCheck) {
+//       namePointerIdx++;
+//       boolCheck = false;
+//     } else {
+//       return false;
+//     }
+//   }
+//   return false;
+// }
 
 console.log('true?', isLongPressedName('alex', 'aaleex'));
 console.log('false?', isLongPressedName('saeed', 'ssaaedd'));
